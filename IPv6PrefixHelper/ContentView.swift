edd5d187f86a2bcd2e9b662559f7881d655075de
Pierre Funke <pifu74@icloud.com>
2025-11-21 11:09:56 +0100

    Initial Commit

---
+++ IPv6PrefixHelper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  IPv6PrefixHelper
+//
+//  Created by Pierre Funke on 21.11.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ IPv6PrefixHelper/IPv6PrefixHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  IPv6PrefixHelperApp.swift
+//  IPv6PrefixHelper
+//
+//  Created by Pierre Funke on 21.11.25.
+//
+
+import SwiftUI
+
+@main
+struct IPv6PrefixHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
